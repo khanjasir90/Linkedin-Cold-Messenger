9da6f9f8fea843aaa03189534a82ab9b90232da6
khanjasir90 <khanmohdjasir@gmail.com>
2022-01-27 12:18:49 +0530

    Added Linkedin Login Template using 3-legged Oauth

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
-app.listen(3000, ()=>  console.log("server is running"))
+app.get('/linkedinLogin', async (req,res,next) => {
+    res.redirect('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id='+process.env.CLIENT_ID+'&redirect_uri='+process.env.CALLBACK_URI+'&state=foobar&scope=r_liteprofile%20r_emailaddress%20w_member_social')
+})
+
+app.get('/',(req,res,next)=>{})
+app.get('/auth/linkedin/callback',(req,res,next)=> {
+
+})
+
+
+app.listen(process.env.PORT,()=> console.log('Server Running on port ',process.env.PORT))
